0b280233def93697071b9784403dac500731c08a
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-01-23 20:27:28 +0800

    add solution (magazine-cutout): update solution

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,6 +1,5 @@
 // This stub file contains items which aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
 
 use std::collections::HashMap;
 
@@ -15,18 +14,16 @@ pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
         *mag_word += 1;
     }
 
+    // Create the note words hashmap
     for word in note {
         let current_word = word.to_string();
         let note_word = notes_map.entry(current_word).or_insert(0);
         *note_word += 1;
     }
 
+    // Get a vector of the words in one map
+    let notes_map_keys: Vec<String> = notes_map.clone().into_keys().collect();
 
-    let mut notes_map_keys: Vec<String> = notes_map.clone().into_keys().collect();
-    let mut magazine_map_keys: Vec<String> = magazine_map.clone().into_keys().collect();
-
-    notes_map_keys.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
-    magazine_map_keys.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
     
     for word in notes_map_keys {
         if !magazine_map.contains_key(&word){
